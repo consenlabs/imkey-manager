b9c8a31ae1e1d98ee045d49b063f80a639b3dd07
Neal Xu <imxuneal@gmail.com>
2020-08-07 11:56:53 +0800

    Add the notarize

--- build/notarize.js
+++ build/notarize.js
@@ -1,4 +1,7 @@
 const { notarize } = require('electron-notarize');
+const dotenv = require('dotenv');
+dotenv.config();
+
 process.env.DEBUG = 'electron-notarize*'
 exports.default = async function notarizing(context) {
     const { electronPlatformName, appOutDir } = context;
@@ -7,14 +10,15 @@ exports.default = async function notarizing(context) {
     }
 
     const appName = context.packager.appInfo.productFilename;
-    console.log("appName:"+appName)
-    const password = '@keychain:serv@imkey.im';
-    console.log("password:"+password)
-    console.log("appName:"+appOutDir)
+    // console.log("appName:"+appName)
+    // const password = '@keychain:serv@imkey.im';
+    // console.log("password:"+password)
+    // console.log("appName:"+appOutDir)
     return await notarize({
         appBundleId: 'com.imkey.imkey-manager',
         appPath: `${appOutDir}/${appName}.app`,
-        appleId: '',
-        appleIdPassword: '',
+        ascProvider: process.env.TEAM_SHORT_NAME,
+        appleId: process.env.APPLE_ID,
+        appleIdPassword: process.env.APPLE_ID_PASSWORD,
     });
 }
